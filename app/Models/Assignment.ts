d2c6e64a8ea3ae9e6898b35ff02af9633a0f66dd
Fajar Rivaldi Chan <fajar.200170188@mhs.unimal.ac.id>
2024-02-28 12:27:37 +0700

    init: assignment model

---
+++ app/Models/Assignment.ts
@@ -0,0 +1,40 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, beforeCreate, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Class from './Class'
+import Mentor from './Mentor'
+import Group from './Group'
+import uuid from 'uuid-wand'
+
+export default class Assignment extends BaseModel {
+  @beforeCreate()
+  public static async createUUID(model: Class) {
+    model.id = uuid.v4()
+  }
+
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public groupId: string | undefined
+
+  @belongsTo(() => Group)
+  public group: BelongsTo<typeof Group>
+
+  @column()
+  public mentorId: string | undefined
+
+  @belongsTo(() => Mentor)
+  public mentor: BelongsTo<typeof Mentor>
+
+  @column()
+  public startTime: DateTime
+
+  @column()
+  public endTime: DateTime
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
