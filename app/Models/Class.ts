2c0c08c3532fd774991225d655e5382dd233b1a2
Fajar Rivaldi Chan <fajar.200170188@mhs.unimal.ac.id>
2024-02-28 10:17:35 +0700

    init: class model

---
+++ app/Models/Class.ts
@@ -0,0 +1,48 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, beforeCreate, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import uuid from 'uuid-wand'
+import Mentor from './Mentor'
+import Category from './Category'
+
+export default class Class extends BaseModel {
+  @beforeCreate()
+  public static async createUUID(model: Class) {
+    model.id = uuid.v4()
+  }
+
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public mentorId: string | undefined
+
+  @belongsTo(() => Mentor)
+  public mentor: BelongsTo<typeof Mentor>
+
+  @column()
+  public categoryId: string | undefined
+
+  @belongsTo(() => Category)
+  public category: BelongsTo<typeof Category>
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column()
+  public price: string
+
+  @column()
+  public thumbnailPath: string
+
+  @column()
+  public grade: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
