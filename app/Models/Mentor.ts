cb7b448c9a671fc5ed4ce37b80bd3a8109fc021b
Fajar Rivaldi Chan <fajar.200170188@mhs.unimal.ac.id>
2024-02-28 10:03:57 +0700

    init: model mentor

---
+++ app/Models/Mentor.ts
@@ -0,0 +1,45 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import uuid from 'uuid-wand'
+
+export default class Mentor extends BaseModel {
+  @beforeCreate()
+  public static async createUUID(model: Mentor) {
+    model.id = uuid.v4()
+  }
+
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public name: string
+
+  @column()
+  public phoneNumber: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public rememberMeToken: string | null
+
+  @column()
+  public image: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(user: Mentor) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
---
+++ database/migrations/1709089218731_mentors.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'mentors'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name').notNullable()
+      table.string('phone_number').notNullable()
+      table.string('email', 255).notNullable().unique().index()
+      table.string('password', 180).notNullable().index()
+      table.string('remember_me_token').nullable()
+      table.string('image')
+      table.boolean('is_active').defaultTo(true)
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
